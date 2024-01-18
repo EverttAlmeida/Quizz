6e5796481761d833bcab5b2099d9918a6c1bc527
Evertt Almeida <108302723+EverttAlmeida@users.noreply.github.com>
2024-01-18 16:43:12 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Quizz from './quizz';
+
+function App() {
+  return (
+    <>
+    <h1>Quizz </h1>
+    
+    <Quizz>
+
+    </Quizz>
+
+    </>    
+  );
+}
+
+export default App;
---
+++ src/data/perguntas.js
@@ -0,0 +1,96 @@
+export const Perguntas  = [
+    {
+        pergunta: 'Qual é o desenho animado sobre um garoto que possui uma arma alienígena chamada "Omnitrix" que lhe dá a habilidade de se transformar em diferentes criaturas?',
+        opcoesResposta: [
+            { resposta: 'Danny Phantom', correta: false, alternativa: 'A)' },
+            { resposta: 'Ben 10', correta: true, alternativa: 'B)'},
+            { resposta: 'Kim Possible', correta: false, alternativa: 'C)' },
+            { resposta: 'Avatar: A Lenda de Aang', correta: false, alternativa: 'D)' },
+        ]   
+    },
+    
+    {
+        pergunta: 'Em "Os Padrinhos Mágicos", qual é o nome do protagonista que tem dois padrinhos mágicos, uma fada e um duende?',
+        opcoesResposta: [
+            { resposta: 'Jimmy Neutron', correta: false, alternativa: 'A)' },
+            { resposta: 'Dexter', correta: false, alternativa: 'B)' },
+            { resposta: 'Timmy Turner', correta: true, alternativa: 'C)' },
+            { resposta: 'Johnny Bravo', correta: false, alternativa: 'D)' },
+        ]
+    }
+    ,
+    {
+        pergunta: 'Qual é o desenho animado que segue as aventuras de três irmãs superpoderosas: Florzinha, Lindinha e Docinho?',
+        opcoesResposta: [
+            { resposta: 'Totally Spies!', correta: false, alternativa: 'A)' },
+            { resposta: 'As Terríveis Aventuras de Billy e Mandy', correta: false, alternativa: 'B)' },
+            { resposta: 'Kim Possible', correta: false, alternativa: 'C)' },
+            { resposta: 'Meninas Superpoderosas', correta: true, alternativa: 'D)' },
+        ]
+    }
+    ,
+    {
+        pergunta: 'Em "Avatar: A Lenda de Aang", qual é o nome do protagonista que pode controlar os quatro elementos?',
+        opcoesResposta: [
+            { resposta: 'Aang', correta: true, alternativa: 'A)' },
+            { resposta: 'Sokka', correta: false, alternativa: 'B)' },
+            { resposta: 'Katara', correta: false, alternativa: 'C)' },
+            { resposta: 'Zuko', correta: false, alternativa: 'D)' },
+        ]
+    }
+    ,
+    {
+        pergunta: 'Qual é o título completo do anime conhecido como "Fullmetal Alchemist"?',
+        opcoesResposta: [
+            { resposta: 'Fullmetal Alchemist: Brotherhood', correta: false, alternativa: 'A)' },
+            { resposta: 'Fullmetal Alchemist: The Elric Brothers', correta: true, alternativa: 'B)' },
+            { resposta: 'Fullmetal Alchemist: Alchemy Chronicles', correta: false, alternativa: 'C)' },
+            { resposta: 'Fullmetal Alchemist: Homunculus Rising', correta: false, alternativa: 'D)' },
+        ]
+    },
+    {
+        pergunta: 'Quem é o criador do anime "Naruto"?',
+        opcoesResposta: [
+            { resposta: 'Akira Toriyama', correta: false, alternativa: 'A)' },
+            { resposta: 'Eiichiro Oda', correta: false, alternativa: 'B)' },
+            { resposta: 'Masashi Kishimoto', correta: true, alternativa: 'C)' },
+            { resposta: 'Hiromu Arakawa', correta: false, alternativa: 'D)' },
+        ]
+    },
+    {
+        pergunta: 'Qual é o nome do animal de estimação de Sailor Moon?',
+        opcoesResposta: [
+            { resposta: 'Artemis', correta: true, alternativa: 'A)' },
+            { resposta: 'Luna', correta: false, alternativa: 'B)' },
+            { resposta: 'Diana', correta: false, alternativa: 'C)' },
+            { resposta: 'Chibi-Usa', correta: false, alternativa: 'D)' },
+        ]
+    },
+    {
+        pergunta: 'Qual é o tipo de criatura que é o personagem principal em "Pokémon"?',
+        opcoesResposta: [
+            { resposta: 'Pokémon', correta: true, alternativa: 'A)' },
+            { resposta: 'Digimon', correta: false, alternativa: 'B)' },
+            { resposta: 'Yokai', correta: false, alternativa: 'C)' },
+            { resposta: 'Tamagotchi', correta: false, alternativa: 'D)' },
+        ]
+    },
+    {
+        pergunta: 'Em "Demon Slayer", qual é a respiração utilizada por Tanjiro Kamado?',
+        opcoesResposta: [
+            { resposta: 'Respiração da Água', correta: false, alternativa: 'A)' },
+            { resposta: 'Respiração do Vento', correta: false, alternativa: 'B)' },
+            { resposta: 'Respiração da Terra', correta: false, alternativa: 'C)' },
+            { resposta: 'Respiração do Fogo', correta: true, alternativa: 'D)' },
+        ]
+    },
+    {
+        pergunta: 'Qual é o nome do personagem principal em "Death Note"?',
+        opcoesResposta: [
+            { resposta: 'L Lawliet', correta: false, alternativa: 'A)' },
+            { resposta: 'Near', correta: false, alternativa: 'B)' },
+            { resposta: 'Light Yagami', correta: true, alternativa: 'C)' },
+            { resposta: 'Misa Amane', correta: false, alternativa: 'D)' },
+        ]
+    },
+]
